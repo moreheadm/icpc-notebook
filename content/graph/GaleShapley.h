4b3b2339379d39cc57d279528f11a08f358eb32c
mjguru <mgurumuk@ucsd.edu>
2020-01-25 14:02:38 -0800

    Updated graph text and added GaleShapley

---
+++ content/graph/GaleShapley.h
@@ -0,0 +1,39 @@
+/**
+ * Author: NUS
+ * Date: 2020-01-25
+ * License: CC0
+ * Source: NUS Notebook
+ * Description: Gale-Shapley algorithm for the stable marriage problem.
+ * madj[i][j] is the jth highest ranked woman for man i.
+ * fpref[i][j] is the rank woman i assigns to man j.
+ * Returns a pair of vectors(mpart, fpart), where mpart[i] gives the partner of man i, and fpart is analogous.
+ * Time: $O(n^2)$
+ */
+
+pair<vector<int>, vector<int>> stable_marriage(vector<vector<int>> & madj, vector<vector<int>>& fpref) {
+	int n = madj.size();
+	vector<int> mpart(n, -1), fpart(n, -1);
+	vector<int> midx(n);
+	queue<int> mfree;
+	for (int i = 0; i < n; i++) {
+		mfree.push(i);
+	}
+	while (!mfree.empty()) {
+		int m = mfree.front();
+		mfree.pop();
+		int f = madj[m][midx[m]++];
+		if (fpart[f] == -1) {
+			mpart[m] = f;
+			fpart[f] = m;
+		} else if (fpref[f][m] < fpref[f][fpart[f]]) {
+			mpart[fpart[f]] = -1;
+			mfree.push(fpart[f]);
+			mpart[m] = f;
+			fpart[f] = m;
+		} else {
+			mfree.push(m);
+		}
+	}
+	return {mpart, fpart};
+}
+
