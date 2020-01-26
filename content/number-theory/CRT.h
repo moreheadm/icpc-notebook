df70cf39b94f591bad4aaea10b05411bcc4f9978
Mohit J Gurumukhani <mgurumuk@its-cseb230-25.ucsd.edu>
2020-01-25 16:47:37 -0800

    Added CRT with vectors, uncommented all geometry algorithms and added text for combinatorial identities. Next, we should write about Prufer sequences. This has not been builded to latest version.

--- content/number-theory/CRT.h
+++ content/number-theory/CRT.h
@@ -7,17 +7,31 @@
  * \texttt{crt(a, m, b, n)} computes $x$ such that $x\equiv a \pmod m$, $x\equiv b \pmod n$.
  * If $|a| < m$ and $|b| < n$, $x$ will obey $0 \le x < \text{lcm}(m, n)$.
  * Assumes $mn < 2^{62}$.
+ * \textttt{crt(x, a)} computes $z$ such that
+ * $z \pmod x_i = a_i \forall i$. Note that the solution is
+ * unique modulo $M = \text{lcm}(x_i)$ . Return (z, M). Note that we do not require the a_i
+ * to be relatively prime.
+ *
  * Status: Works
- * Time: $\log(n)$
+ * Time: $N \log(N)$
  */
 #pragma once
 
 #include "euclid.h"
 
-ll crt(ll a, ll m, ll b, ll n) {
+pair<ll, ll> crt(ll a, ll m, ll b, ll n) {
 	if (n > m) swap(a, b), swap(m, n);
 	ll x, y, g = euclid(m, n, x, y);
 	assert((a - b) % g == 0); // else no solution
 	x = (b - a) % n * x % n / g * m + a;
-	return x < 0 ? x + m*n/g : x;
+	return {x < 0 ? x + m*n/g : x, m*n/g};
+}
+
+pair<ll, ll> crt(vector<int>& x, vector<int>& a) {
+    pair<ll, ll> ret = {a[0], x[0]};
+
+    for(int i = 1;i < int(x.size());i++) {
+        ret = crt(ret.second, ret.first, a[i], x[i]);
+    }
+    return ret;
 }
